d3b5e04d4be1627c368e0bf86dbc8ffb6d0656ae
LuisReyna <larg31@gmail.com>
2017-08-07 15:35:48 -0500

    Add files via upload

---
+++ practicas Arduino/practica 1/led/led.ino
@@ -0,0 +1,15 @@
+
+  int led = 13;
+void setup() {
+
+
+ pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+           // wait for a second
+   
+
+}
